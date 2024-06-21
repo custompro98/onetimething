d82ee113ce9975cb49eee1a71a9f6dee4f502a18
custompro98 <mitchjoa@gmail.com>
2024-06-21 10:13:25 -0400

    feat: Gradient on guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,15 +16,16 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased bg-fuchsia-100">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 sm:mt-[-24rem]">
+<body class="font-sans text-gray-900 antialiased bg-gradient-to-br from-fuchsia-100 via-purple-100 to-fuchsia-200">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
         <div>
             <a href="/" wire:navigate>
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 text-sm sm:text-md bg-white shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
